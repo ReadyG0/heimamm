597f96b9c16408b7ae4531ea4c609a3dc8644b61
ZhouXian <1791434513@qq.com>
2020-02-19 15:28:01 +0800

    项目起步-安装并使用element-ui

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ Vue.config.productionTip = false
 // 把router文件夹的路由文件导入进来
 import router from "./router/index"
 
+// 导入和使用element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
 new Vue({
   // 4. 路由注入
   router,
